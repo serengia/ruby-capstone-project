f66856eb7f0ef207e2194f004eb6705718318503
Rachel Isaac <rachelisaac995@gmail.com>
2023-04-24 18:44:23 +0400

    Fix linter errors

--- book.rb
+++ book.rb
@@ -1,10 +1,10 @@
 require_relative 'label'
 
 class Book
-    attr_reader :id
-    attr_accessor :publisher, :cover_state
+  attr_reader :id
+  attr_accessor :publisher, :cover_state
 
-    def initialize(publisher, cover_state)
+  def initialize(publisher, cover_state)
     @id = Random.rand(1..1000)
     @publisher = publisher
     @cover_state = cover_state
--- label.rb
+++ label.rb
@@ -1,10 +1,10 @@
 require_relative 'item'
 
-class label
-    attr_reader :id
-    attr_accessor :title, :color, :items
+class Label
+  attr_reader :id
+  attr_accessor :title, :color, :items
 
-    def initialize(title, color)
+  def initialize(title, color)
     @id = Random.rand(1..1000)
     @title = title
     @color = color
