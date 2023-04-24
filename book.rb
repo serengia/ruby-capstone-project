d1e01adaf9578387ff0aad481671e31d1f6d3b5c
Rachel Isaac <rachelisaac995@gmail.com>
2023-04-24 19:18:05 +0400

    Add can_be_archived method to book class

--- book.rb
+++ book.rb
@@ -5,8 +5,13 @@ class Book
   attr_accessor :publisher, :cover_state
 
   def initialize(publisher, cover_state)
+    super(genre, author, source, label, publish_date, archived)
     @id = Random.rand(1..1000)
     @publisher = publisher
     @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    super || @cover_state == "bad"
   end
 end
