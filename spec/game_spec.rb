45e934d78b5621c89a23841223d60420e50e2fb3
Ghulam Subbhani <gsmalik030@gmail.com>
2023-04-25 14:14:57 +0200

    Fixed robocop errors

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,26 +1,25 @@
 require_relative '../game/game'
 describe Game do
-describe '#can_be_archived?'do
+  describe '#can_be_archived?' do
     context 'when published over 10 years ago and last played over 2 years ' do
-        game = Game.new(true, Time.new(2020, 1, 1), Time.new(2005, 1, 1))
-        it 'should return true' do
-            expect(game.can_be_archived?).to be_truthy
-        end
+      game = Game.new(true, Time.new(2020, 1, 1), Time.new(2005, 1, 1))
+      it 'should return true' do
+        expect(game.can_be_archived?).to be_truthy
+      end
     end
 
     context 'when published over 10 years ago and last played less than 2 years ' do
-        game = Game.new(true, Time.now, Time.new(2005, 1, 1))
-        it 'should return true' do
-            expect(game.can_be_archived?).to be_falsey
-        end
+      game = Game.new(true, Time.now, Time.new(2005, 1, 1))
+      it 'should return true' do
+        expect(game.can_be_archived?).to be_falsey
+      end
     end
 
     context 'when published less than 10 years ago and last played over 2 years ' do
-        game = Game.new(true, Time.new(2020, 1, 1), Time.now)
-        it 'should return true' do
-            expect(game.can_be_archived?).to be_falsey
-        end
+      game = Game.new(true, Time.new(2020, 1, 1), Time.now)
+      it 'should return true' do
+        expect(game.can_be_archived?).to be_falsey
+      end
     end
-
- end
+  end
 end
