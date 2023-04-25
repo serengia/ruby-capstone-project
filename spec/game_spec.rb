2866098aca3b3c925289f96917cbfede26b20b76
Ghulam Subbhani <gsmalik030@gmail.com>
2023-04-25 13:29:23 +0200

    Added tests for Author class

---
+++ spec/game_spec.rb
@@ -0,0 +1,11 @@
+# require_relative '../game/game'
+# describe Game do
+# describe '#can_be_archived?'do
+#     context 'when published over 10 years ago and last played 2 years ' do
+#         game = Game.new(multiplayer: true, last_played_at: Time.new(2023, 1, 1), publish_date: Time.new(2008, 1, 1) )
+#         it 'should return true' do
+#             expect(game.can_be_archived?).to be_truthy
+#         end
+#     end
+#  end
+# end
