012c3f5e3850411b9ed994046ea81a157ea330e2
Rachel Isaac <rachelisaac995@gmail.com>
2023-04-25 14:34:38 +0400

    Fix linter errors

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -3,21 +3,21 @@ require_relative '../book/label'
 require_relative '../item'
 
 describe Label do
-    before :each do
-        @label = Label.new('label1', 'red')
-    end
+  before :each do
+    @label = Label.new('label1', 'red')
+  end
 
-    it 'has a title' do
-        expect(label.title).to eq('label')
-    end
+  it 'has a title' do
+    expect(label.title).to eq('label')
+  end
 
-    it 'has a color' do
-        expect(label.color).to eq('red')
-    end
+  it 'has a color' do
+    expect(label.color).to eq('red')
+  end
 
-    it 'should return true if the label has an item' do
-        @item = Item.new('2002-01-01')
-        @item.add_label(@label)
-        expect(@label.items).to include(@item)
-    end
+  it 'should return true if the label has an item' do
+    @item = Item.new('2002-01-01')
+    @item.add_label(@label)
+    expect(@label.items).to include(@item)
+  end
 end
