1def9d73d593fe829c4bf74fd4116d1d6d3c3153
Rachel Isaac <rachelisaac995@gmail.com>
2023-04-25 14:19:22 +0400

    Add unit test for book.rb

---
+++ spec/book_spec.rb
@@ -0,0 +1,32 @@
+require_relative '../book/book'
+require_relative '../book/label'
+
+describe Book do
+    before :each do
+        @book = Book.new('publisher', 'good', '2010-01-01')
+    end
+
+    it 'has a publisher' do
+            expect(book.publisher).to eq('publisher')
+    end
+
+    it 'has a cover state' do
+            expect(book.cover_state).to eq('good')
+    end
+
+    it 'has a publish date' do
+            expect(book.publish_date).to eq(time.parse('2010-01-01'))
+    end
+
+    it 'should return true if book is damaged' do
+        @book = Book.new('2011-01-01', 'publisher', 'good')
+        @book.move_to_archive
+        expect(@book.send(:can_be_archived?)).to be true
+    end
+
+    it 'should return true if book is older than 10 years' do
+        @book = Book.new('2002-01-01', 'publisher', 'good')
+        @book.move_to_archive
+        expect(@book.send(:can_be_archived?)).to be true
+    end    
+end
