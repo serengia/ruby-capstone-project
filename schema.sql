47bc842bfc8308f2b0cf2b4b7d908357d3025d6b
Ghulam Subbhani <gsmalik030@gmail.com>
2023-04-26 14:22:03 +0200

    Add schema.sql

---
+++ schema.sql
@@ -0,0 +1,40 @@
+CREATE SCHEMA IF NOT EXISTS catalog_of_my_things;
+
+CREATE TABLE IF NOT EXISTS Genre (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(55) NOT NULL
+  );
+
+CREATE TABLE IF NOT EXISTS Author (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(55) NOT NULL,
+  last_name VARCHAR(55) NOT NULL
+  );
+
+CREATE TABLE IF NOT EXISTS Source (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(55) NOT NULL);
+
+CREATE TABLE IF NOT EXISTS Label (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(55) NOT NULL,
+  color VARCHAR(55) NOT NULL
+  );
+
+CREATE TABLE IF NOT EXISTS archived (
+  id SERIAL PRIMARY KEY,
+  archived BOOLEAN NULL
+);
+
+CREATE TABLE IF NOT EXISTS Game (
+  id SERIAL PRIMARY KEY,
+  multiplayer BOOLEAN NOT NULL,
+  last_played_at DATE NOT NULL,
+  published_date DATE NULL,
+  archived BOOLEAN NULL,
+  Genre_id INT NOT NULL,
+  Author_id INT NOT NULL,
+  Source_id INT NOT NULL,
+  Label_id INT NOT NULL,
+  archived_id INT NOT NULL
+);
