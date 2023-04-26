a9f40e30ccd068183d59862bcc5949ca7a6803e0
James Serengia <jamesserengia@gmail.com>
2023-04-26 06:40:57 +0300

    Add MusicAlbum class unit tests

---
+++ spec/music_album_spec.rb
@@ -0,0 +1,19 @@
+require "../music/music_album"
+
+describe "MusicAlbum" do
+  before :each do
+    @album1 = MusicAlbum.new(true)
+  end
+
+  describe "class" do
+    it "should return value in music_album class" do
+      expect(@album1.on_spotify).to be_truthy
+    end
+  end
+
+  describe "instace" do
+    it "sould be instance of class" do
+      expect(@album1).to be_instance_of MusicAlbum
+    end
+  end
+end
