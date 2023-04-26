9c7f7ec01447bbafa52133b4755ce3d7a503feec
Ghulam Subbhani <gsmalik030@gmail.com>
2023-04-26 11:32:09 +0200

    Added storage folder

--- item.rb
+++ item.rb
@@ -26,4 +26,14 @@ class Item
     @author = author
     author.items.push(self) unless author.items.include?(self)
   end
+
+  def add_label(label)
+    @label = label
+    label.items.push(self) unless label.items.include?(self)
+  end
+
+  def add_genre(genre)
+    @genre = genre
+    genre.items.push(self) unless genre.items.include?(self)
+  end
 end
--- preserve.rb
+++ preserve.rb
@@ -24,11 +24,10 @@ class Preserve
   def load_authors
     return unless File.exist?('authors.json')
 
-    authors_data = JSON.parse(File.read('authors.json'))
-
-    authors_data.each do |item|
-      author = Author.new(item['id'], item['first_name'], item['last_name'])
-      @authors << author
+    authors_loaded = JSON.parse(File.read('authors.json'))
+    authors_loaded.each do |author|
+      new_author = Author.new(author['id'], author['first_name'], author['last_name'])
+      @authors << new_author
     end
   end
 
@@ -36,9 +35,9 @@ class Preserve
     new_game = { id: game.id, publish_date: game.publish_date, multiplayer: game.multiplayer,
                  last_played_at: game.last_played_at, author_id: game.author.id }
     if File.exist?('games.json')
-      games_data = JSON.parse(File.read('games.json'))
-      games_data << new_game
-      File.write('games.json', JSON.pretty_generate(games_data))
+      games_loaded = JSON.parse(File.read('games.json'))
+      games_loaded << new_game
+      File.write('games.json', JSON.pretty_generate(games_loaded))
     else
       File.write('games.json', JSON.pretty_generate([new_game]))
     end
@@ -46,17 +45,13 @@ class Preserve
 
   def save_author(author)
     new_author = { id: author.id, first_name: author.first_name, last_name: author.last_name }
-
-    if File.exist?('authors.json')
-      authors_data = JSON.parse(File.read('authors.json'))
-      authors_data << new_author
-      File.write('authors.json', JSON.pretty_generate(authors_data))
+    if File.exist?('./data/authors.json')
+      authors_loaded = JSON.parse(File.read('./data/authors.json'))
+      authors_loaded << new_author
+      File.write('./data/authors.json', JSON.pretty_generate(authors_loaded))
     else
-      File.write('authors.json', JSON.pretty_generate([new_author]))
+      File.write('./data/authors.json', JSON.pretty_generate([new_author]))
     end
   end
 end
 
-preserve = Preserve.new
-
-preserve.load_games
