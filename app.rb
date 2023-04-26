3c4f776eb205d012a1ec982d236881464e933d67
James Serengia <jamesserengia@gmail.com>
2023-04-26 17:18:55 +0300

    Add music and genre list

--- app.rb
+++ app.rb
@@ -1,9 +1,11 @@
-require 'date'
-require_relative 'preserve'
-require_relative './game/game'
-require_relative './game/author'
-require_relative './books/book'
-require_relative './books/label'
+require "date"
+require_relative "preserve"
+require_relative "./game/game"
+require_relative "./game/author"
+require_relative "./books/book"
+require_relative "./books/label"
+require_relative "./music/genre"
+require_relative "./music/music_album"
 
 class App
   def initialize
@@ -12,10 +14,12 @@ class App
     @preserve.load_games
     @preserve.load_books
     @preserve.load_labels
+    @preserve.load_genres
+    @preserve.load_music_albums
   end
 
   def games_list
-    return puts 'No games found' if @preserve.games.empty?
+    return puts "No games found" if @preserve.games.empty?
 
     @preserve.games.each_with_index do |game, index|
       puts "#{index + 1}) Author: #{game.author.first_name} #{game.author.last_name}"
@@ -24,7 +28,7 @@ class App
   end
 
   def authors_list
-    return puts 'No authors found' if @preserve.authors.empty?
+    return puts "No authors found" if @preserve.authors.empty?
 
     @preserve.authors.each_with_index do |author, index|
       puts "#{index + 1}) First Name: #{author.first_name}, Last Name: #{author.last_name}"
@@ -32,11 +36,11 @@ class App
   end
 
   def add_game
-    puts 'Is it multiplayer [Y / N]'
-    multiplayer = gets.chomp.downcase == 'y'
-    puts 'Enter last played at in format (YYYY-MM-DD)'
+    puts "Is it multiplayer [Y / N]"
+    multiplayer = gets.chomp.downcase == "y"
+    puts "Enter last played at in format (YYYY-MM-DD)"
     last_played_at = Date.parse(gets.chomp)
-    puts 'Enter the publish date in format (YYYY-MM-DD)'
+    puts "Enter the publish date in format (YYYY-MM-DD)"
     publish_date = Date.parse(gets.chomp)
     new_game = Game.new(nil, publish_date, multiplayer, last_played_at)
     puts "Enter author details\n"
@@ -44,15 +48,15 @@ class App
     new_game.add_author(new_author)
     @preserve.games.push(new_game)
     @preserve.save_game(new_game)
-    puts 'Game created successfully'
+    puts "Game created successfully"
   end
 
   private
 
   def add_author
-    puts 'Enter first name'
+    puts "Enter first name"
     first_name = gets.chomp
-    puts 'Enter last name'
+    puts "Enter last name"
     last_name = gets.chomp
     new_author = Author.new(nil, first_name, last_name)
     @preserve.authors.push(new_author)
@@ -62,7 +66,7 @@ class App
 
   # Books
   def books_list
-    return puts 'No books found' if @preserve.books.empty?
+    return puts "No books found" if @preserve.books.empty?
 
     @preserve.books.each_with_index do |book, index|
       puts "#{index + 1}) Publisher: #{book.publisher}, Cover state: #{book.cover_state}"
@@ -70,17 +74,17 @@ class App
   end
 
   def labels_list
-    return puts 'No labels found' if @preserve.labels.empty?
+    return puts "No labels found" if @preserve.labels.empty?
 
     @preserve.labels.each_with_index do |label, index|
       puts "#{index + 1}) Title: #{label.title}, Color: #{label.color}"
     end
   end
 
-    def add_label
-    puts 'Enter title'
+  def add_label
+    puts "Enter title"
     title = gets.chomp
-    puts 'Enter color'
+    puts "Enter color"
     color = gets.chomp
     new_label = Label.new(nil, title, color)
     @preserve.labels.push(new_label)
@@ -89,11 +93,11 @@ class App
   end
 
   def add_book
-    puts 'Is it publisher [Y / N]'
-    publisher = gets.chomp.downcase == 'y'
-    puts 'Enter cover state '
+    puts "Is it publisher [Y / N]"
+    publisher = gets.chomp.downcase == "y"
+    puts "Enter cover state "
     cover_state = gets.chomp
-    puts 'Enter the publish date in format (YYYY-MM-DD)'
+    puts "Enter the publish date in format (YYYY-MM-DD)"
     publish_date = Date.parse(gets.chomp)
     new_book = Book.new(nil, publish_date, publisher, cover_state)
     puts "Enter label details\n"
@@ -101,6 +105,46 @@ class App
     new_book.add_label(new_label)
     @preserve.books.push(new_book)
     @preserve.save_book(new_book)
-    puts 'Book created successfully'
+    puts "Book created successfully"
+  end
+
+  # Music
+  def genre_list
+    return puts "No genres found" if @preserve.genres.empty?
+
+    @preserve.genres.each_with_index do |genre, index|
+      puts "#{index + 1}) Name: #{genre.name}"
+    end
+  end
+
+  def music_albums_list
+    return puts "No music found" if @preserve.music_albums.empty?
+
+    @preserve.music_albums.each_with_index do |album, index|
+      puts "#{index + 1}) Genre: #{album.genre.name} On spotify: #{album.on_spotify} Publish date: #{album.publish_date}"
+    end
+  end
+
+  def add_genre
+    puts "Enter name"
+    name = gets.chomp
+    new_genre = Genre.new(name)
+    @preserve.genres.push(new_genre)
+    @preserve.save_genre(new_genre)
+    new_genre
+  end
+
+  def add_music_album
+    puts "Is it available on Spotify [Y / N]"
+    on_spotify = gets.chomp.downcase == "y"
+    puts "Enter the publish date in format (YYYY-MM-DD)"
+    publish_date = Date.parse(gets.chomp)
+    new_music_album = MusicAlbum.new(nil, publish_date, on_spotify)
+    puts "Enter genre details\n"
+    new_genre = add_genre
+    new_music_album.add_genre(new_genre)
+    @preserve.music_albums.push(new_music_album)
+    @preserve.save_music_album(new_music_album)
+    puts "Music_album created successfully"
   end
 end
